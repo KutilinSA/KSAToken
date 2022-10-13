9fa564afd54dd97b8f80435e60e05ba3c0b91295
Kutilin Sergei <igameskut@icloud.com>
2022-10-13 09:18:54 +0300

    Added goerli build support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const GOERLI_PRIVATE_KEY = 'b7a34dcd511e1b2f43cdb9ec452782c832dca526e760ca29d5652a83e2cde8cc';
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: 'https://goerli.infura.io/v3/3960f94135be47459d172d3e0482eb39',
+      accounts: [GOERLI_PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: 'MZ89MUEMR53UTK84BG8MVV9CHDARDH8FX6',
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,10 @@
 const hre = require("hardhat");
 
 async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
     const KSAToken = await hre.ethers.getContractFactory("KSAToken");
     console.log('Deploying KSAToken...');
     const token = await KSAToken.deploy('KSAToken', 'KSA');
